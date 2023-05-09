caecbd217ae4db6b1256671b1bea75014a84afed
toho36 <toho0036@gmail.com>
2023-05-09 12:17:01 +0200

    adding useRef to get the input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,18 +2,27 @@ import { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
   };
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
